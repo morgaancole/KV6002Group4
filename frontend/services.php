14f54aa09c49f41f4fda2e822e7ca9ede199df26
morgaancole <morgancole2310@gmail.com>
2021-03-03 14:57:15 +0000

    Fixed some screen size bugs

--- frontend/services.php
+++ frontend/services.php
@@ -9,10 +9,14 @@
         <a href="contactForm.php">Contact Us for a quote</a> 
     </h3>
     
-    <div class="wrap">  
-        <div class="item">
+    <div class="service-page">  
+        <div class="service-box">
+            <div class="service-logo">
+                <img class="service-image"src="styles/images/IMG_0150.PNG">
+            </div>
             <h2>Impact Damage Repair</h2>
             <p>We have vast experience dealing with impact damage situations, providing a rapid response to stabilise properties, followed by full reinstatement. Safety and security are paramount in these circumstances and Henderson Building Contractors have the resources to ensure the impacted property is made safe and repaired to its pre-impact condition.</p>
+            <br>
             <p>Impact Damage repair involves a range of services from start to finish such as:</p>
             <ul>
                 <li>Emergency Propping Up</li>
@@ -20,6 +24,85 @@
                 <li>Structural Survey</li>
                 <li>Structural Repair</li>
                 <li>Bricklaying</li>
+            </ul>
+        </div>
+        <div class="service-box">
+            <div class="service-logo">
+                <img class="service-image"src="styles/images/IMG_0148.PNG">
+            </div>
+            <h2>Structure Damage</h2>
+            <p>We carry out various structural repairs including crack repairs, underpinning, drainage, collapse of walls and strengthening works. These repairs are usually carried out under the supervision and to the requirements of a structural engineer. Structural movement can be caused by settlement or accidental damage. Henderson Building Contractors provide services to identify and repair both cause and damage.</p>
+            <br>
+            <p>We offer a range of refurbishment services all under one roof such as:</p>
+            <ul>
+                <li>Groundwork</li>
+                <li>Bricklaying</li>
+                <li>Structural Components Replacement</li>
+                <li>Structural Damage Survey</li>
+                <li>Roofing</li>
+            </ul>
+        </div>
+        <div class="service-box">
+            <div class="service-logo">
+                <img class="service-image"src="styles/images/IMG_0145.PNG">
+            </div>
+            <h2>Water/Oil Escape</h2>
+            <p>Our expertise allows us to carry out escape of water/ oil contracts, working alongside specialist surveyors. Offering services from investigation works through to full reinstatement of damage.</p>
+            <p>Escape of water is caused by water entering a property by the mains water supply, usually through a pipe or tank within the house. Escape of oil is similar to that of water.</p>
+            <p>Henderson Building Contractors have the knowledge and confidence to reinstate the damage to its original specification. Our company strives to achieve the best in customer care, ensuring all work is done efficiently and to the personal needs of the client.</p>
+        </div>
+        <div class="service-box">
+            <div class="service-logo">
+                <img class="service-image"src="styles/images/IMG_0147.PNG">
+            </div>
+            <h2>Fire Damage</h2>
+            <p>Henderson Building contractors offer services ranging from initial emergency/temporary repairs to full fire reinstatement projects. We have experience in domestic, commercial and industrial properties. Fire is capable of causing specific types of structural damage, internal damage and smoke and heat damage. Specialising in this area of work, we are able to assess the best way to repair the damage.</p>
+            <br>
+            <p>Fire Damage repair involves a range of services from start to finish such as:</p>
+            <ul>
+                <li>Stripping Out</li>
+                <li>Contents Removal</li>
+                <li>Thermal Fogging</li>
+                <li>Structural Repairs</li>
+                <li>Roofing</li>
+                <li>Bricklaying</li>
+                <li>Structure Safety Survey</li>
+            </ul>
+        </div>
+        <div class="service-box">
+            <div class="service-logo">
+                <img class="service-image"src="styles/images/IMG_0149.PNG">
+            </div>
+            <h2>Refurbishment</h2>
+            <p>Henderson Building contractors offer refurbishment services to both the commercial and domestic sector, ranging from small private extensions to loft conversions, office refurbishment and dilapidations. Using our skills and experience, all works are project managed to the clients own specification. Services available include groundwork, structural steelwork, windows and door replacement, roofing, mechanical and electrical services, interior refurbishment and full decoration.</p>
+            <br>
+            <p>We offer a range of refurbishment services under one roof such as:</p>
+            <ul>
+                <li>Groundwork</li>
+                <li>Structural Steelwork</li>
+                <li>Window/Door Replacement</li>
+                <li>Roofing</li>
+                <li>Mechanical and Electrical Services</li>
+                <li>Interior Refurbishment</li>
+                <li>Full Decoration</li>
+            </ul>
+        </div>
+        <div class="service-box">
+            <div class="service-logo">
+                <img class="service-image"src="styles/images/IMG_0144.PNG">
+            </div>
+            <h2>Flood Damage</h2>
+            <p>Our Company has been involved in projects as a result of all recent large scale flooding incidents in the North East region of England. We are prepared to offer immediate services to the industry when unforeseen natural events occur. We ensure our expertise and workforce are readily available during such unpredictable times.</p>
+            <br>
+            <p>Water Damage Services Include: </p>
+            <ul>
+                <li>Stripping Out</li>
+                <li>Contents Removal</li>
+                <li>Drying Out</li>
+                <li>Structural Repairs</li>
+                <li>Roofing</li>
+                <li>Electrical Works</li>
+                <li>Plumbing/Heating Services</li>
             </ul>
         </div>
     </div>
