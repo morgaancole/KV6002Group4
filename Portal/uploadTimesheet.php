c61a6761ea6f8ada8b298fc1754a01d303ab15be
LiamDavison <78809536+LiamDavison@users.noreply.github.com>
2021-03-16 14:35:04 +0000

    commit

--- Portal/uploadTimesheet.php
+++ Portal/uploadTimesheet.php
@@ -33,16 +33,17 @@ function handleUpload()
     $desc = $_POST['desc'];
     $sanitizedDesc = sanitizeInput($desc);
 
+    $timesheetID = generateRandomId();
+    // echo $timesheetID;
+
     $conn = makeConnection();
-    $stmt = $conn->prepare("INSERT INTO hd_timesheet_responses (staff_id, Date, location, hours_worked, jobs_completed_desc, overtime_worked)
-        VALUES(:id, :date, :location, :hoursworked, :desc, :overTime)");
-    $params = ["id" => $sanitizedId, "date" => $date, "location" => $sanitizedLocation, "hoursworked" => $sanitizedHours, "desc" => $sanitizedDesc, "overTime" => $sanitizedHoursOver];
+    $stmt = $conn->prepare("INSERT INTO hd_timesheet_responses (timesheet_id, staff_id, Date, location, hours_worked, jobs_completed_desc, overtime_worked)
+        VALUES( :tid ,:id, :date, :location, :hoursworked, :desc, :overTime)");
+    $params = ["tid" => $timesheetID, "id" => $sanitizedId, "date" => $date, "location" => $sanitizedLocation, "hoursworked" => $sanitizedHours, "desc" => $sanitizedDesc, "overTime" => $sanitizedHoursOver];
 
     $result = $stmt->execute($params);
 
     if ($result) {
-
-        
 
         $stmt = $conn->prepare("select hourly_rate, staff_first_name from hd_staff_users join hd_pay_categories on (hd_staff_users.pay_id = hd_pay_categories.pay_id) where staff_id = $sanitizedId");
         $params = [];
@@ -62,20 +63,19 @@ function handleUpload()
 
         $postTax = $preTax * 0.80;
 
-        $stmt = $conn->prepare("insert into hd_payslips (staff_id, hours_worked, salary, overtime_worked, pre_tax_income, post_tax_income, deductables,  final_income, process_id )
-         VALUES (:sid, :hours, :sal, :over, :pre, :post, :deductables, :final, :process)");
-         $params = ["sid" => $sanitizedId, "hours" => $sanitizedHours, "sal" => $salaryReg, "over" => $sanitizedHoursOver , "pre" => $preTax,
-        "post" => $postTax, "deductables" => 0, "final" => $postTax, "process" => 1  ];
+        $stmt = $conn->prepare("insert into hd_payslips (staff_id, hours_worked, salary, overtime_worked, pre_tax_income, post_tax_income, deductables,  final_income, process_id, timesheet_id )
+         VALUES (:sid, :hours, :sal, :over, :pre, :post, :deductables, :final, :process, :tid)");
+        $params = ["sid" => $sanitizedId, "hours" => $sanitizedHours, "sal" => $salaryReg, "over" => $sanitizedHoursOver, "pre" => $preTax,
+            "post" => $postTax, "deductables" => 0, "final" => $postTax, "process" => 1, "tid" => $timesheetID];
         $result = $stmt->execute($params);
-        
 
-        if($result) {
+        if ($result) {
             require_once "inc/functions.php";
             echo makePageStart("Timesheet");
             echo createPageBody();
-    
+
             $success = <<<UPLOADED
-    
+
             <div class="success_outer">
             <div class="success_inner">
             <img class="success_img" src="img/success.png" alt="success tick">
@@ -83,21 +83,14 @@ function handleUpload()
                 <a href="dash.php"><button>Home</a></button>
                 </div>
             </div>
-    
+
 UPLOADED;
             $success .= "\n";
             echo $success;
             echo createPageClose();
-        } 
-
-
-
-        
-
+        }
 
         //upload to payslips table with the calculations
-
-        
 
     } else {
         echo "not submitted";
@@ -120,4 +113,16 @@ function sanitizeInput($val)
     $santiseVal = stripslashes($santiseVal);
     return $santiseVal;
 }
-?>
+
+function generateRandomId()
+{
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $randomString = '';
+
+    for ($i = 0; $i < 6; $i++) {
+        $index = rand(0, strlen($characters) - 1);
+        $randomString .= $characters[$index];
+    }
+
+    return $randomString;
+}
