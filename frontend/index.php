7a0f6aed8a12bffc2cc049dcb3b704a042c02a71
rachel-e-johnson <61086133+rachel-e-johnson@users.noreply.github.com>
2021-03-31 12:33:44 +0100

    31.3

--- frontend/index.php
+++ frontend/index.php
@@ -8,29 +8,96 @@
 <body>
     <h3 class="title">Welcome to Henderson Building Contractors</h3>
     
-  <img src="images/saw.jpg" alt="Builders" style="width:100%;">
+<div class="content-wrapper">
+    <div class="portfolio-items-wrapper">
+        
+        <div class="portfolio-item-wrapper">
+                <div class="portfolio-img-background" style="background-image:url(images/blueprint.jpeg)"></div>
+            
+            <div class="img-text-wrapper">
+                
+                <div class="subtitle">
+                    Testingggg
+                </div>
+                
+            </div>
+            
+        </div>
+        
+        <div class="portfolio-item-wrapper">
+                <div class="portfolio-img-background" style="background-image:url(images/builder.jpeg)"></div>
+            
+            <div class="img-text-wrapper">
+                
+                <div class="subtitle">
+                    Testingggg1
+                </div>
+                
+            </div>
+            
+        </div>
+        
+        <div class="portfolio-item-wrapper">
+                <div class="portfolio-img-background" style="background-image:url(images/extension.jpeg)"></div>
+            
+            <div class="img-text-wrapper">
+                
+                <div class="subtitle">
+                    Testingggg2
+                </div>
+                
+            </div>
+            
+        </div>
+        
+        <div class="portfolio-item-wrapper">
+                <div class="portfolio-img-background" style="background-image:url(images/paper.jpeg)"></div>
+            
+            <div class="img-text-wrapper">
 
-<div class="about">
+                <div class="subtitle">
+                    Testingggg3
+                </div>
+                
+            </div>
+            
+        </div>
+        
+        <div class="portfolio-item-wrapper">
+                <div class="portfolio-img-background" style="background-image:url(images/people.jpeg)"></div>
+            
+            <div class="img-text-wrapper">
+ 
+                <div class="subtitle">
+                    Testingggg4
+                </div>
+                
+            </div>
+            
+        </div>
+        
+        <div class="portfolio-item-wrapper">
+                <div class="portfolio-img-background" style="background-image:url(images/saw.jpg)"></div>
+            
+            <div class="img-text-wrapper">
+
+                <div class="subtitle">
+                    Testingggg5
+                </div>
+                
+            </div>
+            
+        </div>
+        
+    </div>
+</div>  
     
-    <h2>Intro</h2>
-    <br>
-    <p>-Intro to business-</p>
-    <br>
-    <h2>Services</h2>
-    <br>
-    <p>-Link to services page-</p>
-    <br>
-    <h2>Previous work</h2>
-    <br>
-    <p>-Image show of previous work-</p>
-    <br>
-    <h2>Reviews</h2> 
-    <br>
-    <p>-Customer reviews from database-</p>
-    
-</div>
 
     
+    
+    
+    
+
 
 </body>
 
--- frontend/logout.php
+++ frontend/logout.php
@@ -2,3 +2,4 @@
 session_start(); //start session
 session_destroy(); //destroy session
 header("location: index.php"); //relocate to logged out home page
+?>
