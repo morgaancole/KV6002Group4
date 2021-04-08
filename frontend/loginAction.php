acca3530297bf48a0ace71d24d5d246ecd4f3e9a
rachel-e-johnson <61086133+rachel-e-johnson@users.noreply.github.com>
2021-04-08 13:50:45 +0100

    login

--- frontend/loginAction.php
+++ frontend/loginAction.php
@@ -41,18 +41,20 @@
             $_SESSION['logged-in'] = 'true'; 
             $_SESSION ['email'] = $email; 
 
-            
- 
             header('Location: ../Portal/dash.php');
-        }else{
-            header('Location: loginFail.php'); 
+        }
+           
+            else {
+               header('Location: loginFail.php'); 
         } 
-    }else if ($admin) {  
+    }
+            
+            else if ($admin) {  
             
             $passwordHash = $admin['admin_password'];
 
 
-        if(password_verify($password, $passwordHash)){  
+         if(password_verify($password, $passwordHash)){  
             
             $_SESSION['logged-in'] = true; 
             $_SESSION ['email'] = $email; 
@@ -61,17 +63,21 @@
             header('Location: ../Portal/adminDashboard.php');
 
                
-        }else{
-          header('Location: loginFail.php');       
         }
-    }else{    
-        echo "user not found";
+                
+            else{
+                header('Location: loginFail.php');       
+        }
+    }
+            else{    
+                echo "user not found";
       } 
          
             
             
-     } catch (Exception $e) {
-            echo "There was a problem: " . $e->getMessage();
+     } 
+            catch (Exception $e) {
+                echo "There was a problem: " . $e->getMessage();
     }
     
     }
